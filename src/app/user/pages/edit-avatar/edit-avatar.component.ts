6a86a18c8c5680986c23cd1870145bbdff75f4e9
JulioCGonzalez <jcgonzalez_95@hotmail.com>
2024-03-19 20:51:54 -0600

    adding the avatar selection image and some validation

--- src/app/user/pages/edit-avatar/edit-avatar.component.ts
+++ src/app/user/pages/edit-avatar/edit-avatar.component.ts
@@ -1,87 +1,98 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { User } from '../../../auth/interfaces/user.interface';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Avatar } from '../../../shared/models/avatar';
 import { AvatarService } from '../../../shared/services/avatar.service';
-import { FormBuilder, FormGroup } from '@angular/forms';
 import { AuthService } from '../../../auth/services/auth.service';
 import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-edit-avatar',
   templateUrl: './edit-avatar.component.html',
-  styleUrl: './edit-avatar.component.css'
+  styleUrls: ['./edit-avatar.component.css']
 })
-export class EditAvatarComponent implements OnInit{
+export class EditAvatarComponent implements OnInit {
   avatar?: Avatar = undefined;
-  avatars: any[] =[];
-  avatarInfo?: any =  { name: 'Mujer', url: 'female.png' };
-  constructor(private router: Router,
+  avatars: any[] = [];
+  avatarInfo?: any = { name: 'Mujer', url: 'female.png' };
+  editForm: FormGroup;
+
+  constructor(
+    private router: Router,
     private fb: FormBuilder,
     private avatarService: AvatarService,
-    private authService: AuthService, ) {
+    private authService: AuthService
+  ) {
     this.avatars = [
       { name: 'Hombre', url: 'male.png' },
       { name: 'Mujer', url: 'female.png' },
     ];
     this.avatar = this.router.getCurrentNavigation()?.extras.state;
+    this.editForm = this.fb.group({
+      full_name: ['', Validators.required],
+      pin: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(6)]],
+      age: ['', Validators.required],
+      avatar: ['']
+    });
   }
 
-  public editForm: FormGroup = this.fb.group({});
- 
   ngOnInit(): void {
     this.createEditFormGroup();
   }
 
-  onEdit(){
+  onEdit() {
     if (this.editForm.valid) {
-      let avatarData: Avatar = this.editForm.value;
-      avatarData.user_id = this.authService.currentUserLog?.id;
-      this.avatarService.editAvatar(avatarData).subscribe({
-        next: value =>{
-          Swal.fire({
-            title: "Listo!",
-            text: "El avatar se edito correctamente!",
-            icon: "success",
-            confirmButtonColor: "#3085d6",
-            confirmButtonText: "Ok"
-          }).then((result) => {
-            this.router.navigate(['/users/avatar']);
-          });
-        },
-        error: err => {
-          console.error('Observable emitted an error: ' + err);
-          Swal.fire({
-            icon: "error",
-            title: "Oops...",
-            text: "El avatar no se pudo editar",
-          });
-        }
-      })
+      if (this.avatar && this.authService.currentUserLog) {
+        let avatarData: Avatar = this.editForm.value;
+        avatarData.user_id = this.authService.currentUserLog.id;
+        avatarData.id = this.avatar.id;
+        this.avatarService.editAvatar(avatarData).subscribe({
+          next: value => {
+            Swal.fire({
+              title: "Listo!",
+              text: "El avatar se editó correctamente!",
+              icon: "success",
+              confirmButtonColor: "#3085d6",
+              confirmButtonText: "Ok"
+            }).then((result) => {
+              this.router.navigate(['/users/avatar']);
+            });
+          },
+          error: err => {
+            console.error('Observable emitted an error: ' + err);
+            Swal.fire({
+              icon: "error",
+              title: "Oops...",
+              text: "El avatar no se pudo editar",
+            });
+          }
+        });
+      } else {
+        console.error("No se puede editar el avatar: el avatar o el usuario actual son nulos.");
+      }
     }
   }
 
   createEditFormGroup(): void {
-    this.editForm = this.fb.group({
-      full_name:[this.avatar!.full_name],
-      pin:[this.avatar!.pin],
-      avatar:[this.avatar!.avatar],
-      age:[this.avatar!.age],
-      id:[this.avatar!.id],
-      user_id:[this.avatar!.user_id],
+    this.editForm.patchValue({
+      full_name: this.avatar?.full_name,
+      pin: this.avatar?.pin,
+      age: this.avatar?.age,
+      avatar: this.avatar?.avatar
     });
+
     if (this.avatar?.avatar) {
       this.avatarInfo = this.avatars.find(avatar => avatar.url === this.avatar?.avatar);
     }
   }
 
-  onClickBack(event: any){
+  onClickBack(event: any) {
     this.router.navigate(['/users/avatar']);
   }
 
-  onDelete(event: any){
+  onDelete(event: any) {
     Swal.fire({
-      title: `El avatar ${this.avatar?.full_name} se eliminara, esta usted seguro?`,
+      title: `El avatar ${this.avatar?.full_name} se eliminará, ¿está seguro?`,
       text: "¡No podrás revertir esto!",
       icon: "warning",
       showCancelButton: true,
@@ -95,12 +106,12 @@ export class EditAvatarComponent implements OnInit{
     });
   }
 
-  deleteAvatar(){
+  deleteAvatar() {
     this.avatarService.deleteAvatar(this.avatar?.id!).subscribe({
-      next: value =>{
+      next: value => {
         Swal.fire({
           title: "Listo!",
-          text: "El avatar se elimino correctamente!",
+          text: "El avatar se eliminó correctamente!",
           icon: "success",
           confirmButtonColor: "#3085d6",
           confirmButtonText: "Ok"
@@ -116,6 +127,6 @@ export class EditAvatarComponent implements OnInit{
           text: "El avatar no se pudo eliminar",
         });
       }
-    })
+    });
   }
 }
--- src/app/user/pages/register-avatar/register-avatar.component.ts
+++ src/app/user/pages/register-avatar/register-avatar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 import { AvatarService } from '../../../shared/services/avatar.service';
 import { Avatar } from '../../../shared/models/avatar';
 import { Router } from '@angular/router';
@@ -9,59 +9,73 @@ import Swal from 'sweetalert2';
 @Component({
   selector: 'app-register-avatar',
   templateUrl: './register-avatar.component.html',
-  styleUrl: './register-avatar.component.css'
+  styleUrls: ['./register-avatar.component.css']
 })
 export class RegisterAvatarComponent implements OnInit {
   public registerForm: FormGroup = this.fb.group({
-    full_name:['', [Validators.required]],
-    pin:[''],
-    avatar:[''],
-    age:['']
+    full_name: ['', [Validators.required, Validators.minLength(3)]], // Validación para el nombre
+    pin: ['', [pinLengthValidator]], // Validación para el pin
+    avatar: [''], // No es necesario Validators.required aquí
+    age: ['', [Validators.required, Validators.max(15)]] // Cambio en la validación de edad
   });
 
   avatars: any[] =[];
 
-  constructor( private fb: FormBuilder,
+  constructor(
+    private fb: FormBuilder,
     private avatarService: AvatarService,
     private authService: AuthService,
-    private router: Router){}
+    private router: Router
+  ) {}
 
-    ngOnInit(): void {
-      this.avatars = [
-        { name: 'Hombre', url: 'male.png' },
-        { name: 'Mujer', url: 'female.png' },
-      ];
-    }
+  ngOnInit(): void {
+    this.avatars = [
+      { name: 'Hombre', url: 'male.png' },
+      { name: 'Mujer', url: 'female.png' },
+    ];
+  }
 
-    get fg() {
-      return this.registerForm.controls;
-    }
+  get fg() {
+    return this.registerForm.controls;
+  }
 
-    onRegister(){
-      if (this.registerForm.valid) {
-        let avatarData: Avatar = this.registerForm.value;
-        avatarData.user_id = this.authService.currentUserLog?.id;
-        this.avatarService.registerAvatar(avatarData).subscribe({
-          next: value =>{
-            Swal.fire({
-              title: "Listo!",
-              text: "El avatar se registro correctamente!",
-              icon: "success",
-              confirmButtonColor: "#3085d6",
-              confirmButtonText: "Ok"
-            }).then((result) => {
-              this.router.navigate(['/users/avatar']);
-            });
-          },
-          error: err => {
-            console.error('Observable emitted an error: ' + err);
-            Swal.fire({
-              icon: "error",
-              title: "Oops...",
-              text: "El avatar no se pudo registrar",
-            });
-          }
-        })
-      }
+  selectAvatar(avatarUrl: string) {
+    this.registerForm.patchValue({ avatar: avatarUrl });
+  }
+
+  onRegister() {
+    if (this.registerForm.valid) {
+      let avatarData: Avatar = this.registerForm.value;
+      avatarData.user_id = this.authService.currentUserLog?.id;
+      this.avatarService.registerAvatar(avatarData).subscribe({
+        next: value => {
+          Swal.fire({
+            title: 'Listo!',
+            text: 'El avatar se registró correctamente!',
+            icon: 'success',
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'Ok'
+          }).then((result) => {
+            this.router.navigate(['/users/avatar']);
+          });
+        },
+        error: err => {
+          console.error('Observable emitted an error: ' + err);
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'El avatar no se pudo registrar',
+          });
+        }
+      });
     }
+  }
+}
+
+function pinLengthValidator(control: AbstractControl): { [key: string]: boolean } | null {
+  const pin = control.value;
+  if (pin && pin.toString().length !== 6) {
+    return { 'invalidPinLength': true };
+  }
+  return null;
 }
